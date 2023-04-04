fb5073e6dbc8848e6bb5ca02969abddf09eb1ad3
Kasun Dulara <kasundularaam@gmail.com>
2023-04-04 16:31:19 +0530

    users page redesigned with MUI

--- app/(main)/users/(components)/UserCard.tsx
+++ app/(main)/users/(components)/UserCard.tsx
@@ -1,11 +1,42 @@
+import {
+  Avatar,
+  Card,
+  CardContent,
+  createTheme,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
+import { green } from "@mui/material/colors";
 import React from "react";
 import { UserCardProps } from "../(componentProps)/UserCardProps";
+
 const UserCard: React.FC<UserCardProps> = ({ user }) => {
+  const theme = createTheme({
+    palette: {
+      mode: "dark",
+      primary: {
+        main: green[500],
+      },
+    },
+  });
   return (
-    <div className="flex flex-col p-3 bg-gray-700 rounded-md mb-2 mx-5 border-gray-500 border">
-      <p>{user.name ?? "No Name"}</p>
-      <p>{user.email}</p>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Card sx={{ maxWidth: 400, m: 2, width: "100%" }}>
+        <CardContent sx={{ display: "flex", alignItems: "center" }}>
+          <Avatar sx={{ bgcolor: "primary.main", mr: 2 }}>
+            {user.name.charAt(0).toUpperCase()}
+          </Avatar>
+          <div>
+            <Typography variant="h5" component="h2" gutterBottom>
+              {user.name}
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              <strong>Email:</strong> {user.email}
+            </Typography>
+          </div>
+        </CardContent>
+      </Card>
+    </ThemeProvider>
   );
 };
 
--- app/(main)/users/page.tsx
+++ app/(main)/users/page.tsx
@@ -1,4 +1,18 @@
+"use client";
+
 import { User } from "@/domain/auth/User";
+import {
+  Box,
+  Container,
+  createTheme,
+  CssBaseline,
+  styled,
+  Theme,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
+import { green } from "@mui/material/colors";
+import { useEffect, useState } from "react";
 import UserCard from "./(components)/UserCard";
 
 const getUsers = async () => {
@@ -7,17 +21,56 @@ const getUsers = async () => {
   return data as User[];
 };
 
-export default async function Users() {
-  const users = await getUsers();
+const UserListWrapper = styled("div")(({ theme }) => ({
+  backgroundColor: theme.palette.background.default,
+  color: theme.palette.text.primary,
+  minHeight: "100vh",
+}));
+
+export default function Users() {
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    async function load() {
+      const list = await getUsers();
+      setUsers(list);
+    }
+    load();
+  });
+
+  const theme = createTheme({
+    palette: {
+      mode: "dark",
+      primary: {
+        main: green[500],
+      },
+    },
+  });
+
+  const styles = {
+    userListContainer: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+  };
 
   return (
-    <div className="bg-gray-800 h-screen text-gray-300 flex flex-col gap-5 pt-5">
-      <h1 className="text-green-500 text-center font-bold text-3xl">Users</h1>
-      <div>
-        {users.map((user) => {
-          return <UserCard key={user.email} user={user} />;
-        })}
-      </div>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Typography
+          variant="h4"
+          align="center"
+          sx={{ color: "#4CAF50", py: 4 }}
+        >
+          Users
+        </Typography>
+        <Box display="flex" flexDirection="column" alignItems="center">
+          {users.map((user) => (
+            <UserCard key={user._id} user={user} />
+          ))}
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
